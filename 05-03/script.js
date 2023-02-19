709c73b3a1e3e36bcb8c7762c190c027fa19f984
adlihardiyanto <testingerkamv2@gmail.com>
2023-02-19 12:25:50 +0700

    Update

--- 05-03/script.js
+++ 05-03/script.js
@@ -8,13 +8,59 @@ const biodataPemain = {
     posisi: 'Gelandang',
 }
 
-console.log(biodataPemain?.nama);
-console.log(biodataPemain?.name);
+// Jika Tanpa Fungsi
+const validasiKey6 = 'nama' in biodataPemain;
 
-function validasiKey (key) {
-    if (biodataPemain?.nama !== undefined) {
-        return 'Key ada di dalam object Biodata Pemain'
+if(validasiKey6) {
+    console.log('Ada Kok')
+} else {
+    console.log('Gak Ada')
+}
+
+// Pake Fungsi
+const validasiKey4 = (objectName, keyName) => {
+    let adaKeyNih = Object.keys(objectName).some(key => key === keyName);
+    return adaKeyNih;
+}
+
+console.log(validasiKey4(biodataPemain, 'name'))
+
+// Dibuat function
+const hasKey = (objectName, keyName) => {
+    if(Object.keys(objectName).indexOf(keyName) === 1) {
+        return 'Ada Kok'
+    } else {
+        return 'Gak Ada'
     }
 }
 
-console.log(validasiKey())
+console.log(hasKey(biodataPemain, 'age'));
+
+// Cara Simple
+const validasiKey5 = (objectName, keyName) => {
+    if(objectName[keyName]){
+        return 'Ada Kok'
+    }
+    else{
+        return 'Gak Ada'
+    }
+}
+
+console.log(validasiKey5(biodataPemain, 'negara'));
+
+
+// Soal No. 2 -> Menjumlahkan value di object tetapi hanya yang number
+const angka = {
+    x : 'abc',
+    y : 10,
+    z : 20,
+}
+
+let sum = 0;
+for (let key in angka) {
+    if (typeof angka[key] === 'number') {
+        sum += angka[key];
+    }
+}
+
+console.log(sum)
